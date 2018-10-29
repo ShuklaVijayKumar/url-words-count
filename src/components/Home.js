5e02ee886200ac2d1e544a3df9f4eab276bb0fa5
DESKTOP-CI4MT01\shukl <shukla.vijay@outlook.com>
2018-10-29 21:30:10 +0000

    Removed unused references.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,4 @@
 import React from "react";
-import {
-  Form,
-  FormControl,
-  FormGroup,
-  ControlLabel,
-  Button
-} from "react-bootstrap";
 
 class Home extends React.Component {
   constructor(props, context) {
@@ -18,14 +11,6 @@ class Home extends React.Component {
     };
   }
 
-  getValidationState() {
-    const length = this.state.value.length;
-    if (length > 10) return "success";
-    else if (length > 5) return "warning";
-    else if (length > 0) return "error";
-    return null;
-  }
-
   handleChange(e) {
     this.setState({ value: e.target.value });
   }
@@ -33,21 +18,27 @@ class Home extends React.Component {
   render() {
     return (
       <div className="container-fluid" id="contact">
-      
         <div className="row">
           <form className="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-lg-offset-3 col-md-offset-3 ">
             <div className="form-group">
-              <label for="dessertPicture">Please enter url below to calculate the word occurances</label>
-              <input type="text" className="form-control" id="dessertPicture" placeholder="https://en.wikipedia.org/wiki/WisdomTree_Investments"/>
+              <label>
+                Please enter url below to calculate the word occurances
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="urlText"
+                placeholder="https://en.wikipedia.org/wiki/WisdomTree_Investments"
+              />
             </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
           </form>
         </div>
       </div>
     );
   }
 }
-
-//render(<Stats />);
 
 export default Home;
