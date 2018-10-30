5c7ac62af765b69aef94a006fca266611a92dc3f
ShuklaVijayKumar <shukla.vijay@outlook.com>
2018-10-30 17:16:52 +0000

    Code cleanup

--- src/client/src/components/Home.js
+++ src/client/src/components/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
+import Stats from "./Stats";
 
-class Home extends React.Component {
+class Page1 extends React.Component {
   constructor(props, context) {
     super(props, context);
-
     this.handleChange = this.handleChange.bind(this);
-
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
-      value: ""
+      submited: ""
     };
   }
 
@@ -15,11 +15,22 @@ class Home extends React.Component {
     this.setState({ value: e.target.value });
   }
 
+  handleSubmit(event) {
+    fetch("https://jsonplaceholder.typicode.com/albums")
+      .then(response => response.json())
+      .then(data => this.setState({ submitted:true, data:data }));
+      //console.log(this.state.data);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="container-fluid" id="contact">
         <div className="row">
-          <form className="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-lg-offset-3 col-md-offset-3 ">
+          <form
+            className="col-lg-6 col-md-offset-3"
+            onSubmit={this.handleSubmit}
+          >
             <div className="form-group">
               <label>
                 Please enter url below to calculate the word occurances
@@ -34,6 +45,9 @@ class Home extends React.Component {
             <button type="submit" className="btn btn-primary">
               Submit
             </button>
+            <div>
+              <Stats result={this.state.data}/>
+            </div>
           </form>
         </div>
       </div>
@@ -41,4 +55,4 @@ class Home extends React.Component {
   }
 }
 
-export default Home;
+export default Page1;
